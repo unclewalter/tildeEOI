8af02a15678aea5298ed42ad12b907f0e4f60801
Kevan Atkins <kevan.atkins@gmail.com>
2016-08-16 23:01:21 +1000

    Fixing PDF output. Turns out feature can't be tested locally with lambda-local. :-(

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 // Loading Dependencies
-var aws = require('aws-sdk'),
+var aws    = require('aws-sdk'),
     crypto = require('crypto'),
-    fs = require('fs'),
-    pdf = require('pdfkit');
+    fs     = require('fs'),
+    pdf    = require('pdfkit'),
+    spindrift = require('spindrift');
 
 // Loading in the secret stuff
 aws.config.loadFromPath('./aws-config.json');
 
 // Amazon services
-var s3 = new aws.S3({});
+// var s3  = new aws.S3({});
 var ses = new aws.SES({});
+
+// TODO: Move to utility functions file.
+var getFilesizeInBytes = function(filename) {
+  var stats = fs.statSync(filename)
+  var fileSizeInBytes = stats["size"]
+  return fileSizeInBytes
+}
 
 /*
 Function Name: processSubmission
@@ -22,21 +30,193 @@ Process:
 5. Pushes the PDF file to S3.
 */
 var processSubmission = function(submissionDetails, context) {
-  var hash = crypto.createHash('md5').update(submissionDetails.email).digest("hex");
-  var filename = './tmp/' + hash + '_' + submissionDetails.timestamp + '.pdf';
+  var hash        = crypto.createHash('md5').update(submissionDetails.email).digest("hex");
+  var filename    = hash + '_' + submissionDetails.timestamp + '.pdf';
+  var filepath    = '/tmp/' + filename;
 
-  var myDoc = new pdf;
+  // Initializing objects
+  var myDoc       = new pdf({
+    size: 'A4',
+    margins: {
+      top:    50,
+      bottom: 50,
+      left:   72,
+      right:  72
+    }
+  });
+  var writeStream = fs.createWriteStream(filepath);
+  var s3obj       = new aws.S3(
+        {
+    params: {
+      Bucket: 'tilde-submissions',
+      Key: 'eoi/' + filename
+    }
+  });
 
-  myDoc.pipe(fs.createWriteStream(filename));
-  myDoc.font('Times-Roman')
-      .fontSize(50)
-      .text('PDF Test', 100, 100);
+  // Generate PDF file
+  myDoc.pipe(writeStream);
+
+  myDoc.image('./images/tilde-logo.png', myDoc.page.width-80, 30, {
+    width: 50
+  });
+  myDoc.font('./fonts/Sanchez-Regular.ttf').fontSize(35);
+
+  myDoc.text('EOI: '+submissionDetails.project_title, {
+    align: 'center',
+    width: (myDoc.page.width-72-90)
+  });
+
+  myDoc.moveDown();
+
+  // Short Description
+
+  myDoc.font('./fonts/Sanchez-Italic.ttf').fontSize(20);
+  myDoc.text('Short Description: ', {
+    align: 'left'
+  });
+  myDoc.font('./fonts/Questrial-Regular.ttf').fontSize(12);
+  myDoc.text(submissionDetails.short_description, {
+    align: 'justify'
+  });
+  myDoc.moveDown(0.5);
+
+  // Long Description
+
+  myDoc.font('./fonts/Sanchez-Italic.ttf').fontSize(20);
+  myDoc.text('Long Description: ', {
+    align: 'left'
+  });
+  myDoc.font('./fonts/Questrial-Regular.ttf').fontSize(12);
+  myDoc.text(submissionDetails.long_description, {
+    align: 'justify'
+  });
+  myDoc.moveDown(0.5);
+
+  // Technical Requirements
+
+  myDoc.font('./fonts/Sanchez-Italic.ttf').fontSize(20);
+  myDoc.text('Technical Requirements: ', {
+    align: 'left'
+  });
+  myDoc.font('./fonts/Questrial-Regular.ttf').fontSize(12);
+  myDoc.text(submissionDetails.technical_requirements, {
+    align: 'justify'
+  });
+  myDoc.moveDown(0.5);
+
+  // Instrumentation
+
+  myDoc.font('./fonts/Sanchez-Italic.ttf').fontSize(20);
+  myDoc.text('Instrumentation (if applicable): ', {
+    align: 'left'
+  });
+  myDoc.font('./fonts/Questrial-Regular.ttf').fontSize(12);
+  myDoc.text(submissionDetails.instrumentation, {
+    align: 'justify'
+  });
+  myDoc.moveDown(0.5);
+
+  // Funding
+
+  myDoc.font('./fonts/Sanchez-Italic.ttf').fontSize(20);
+  myDoc.text('Funding:', {
+    align: 'left'
+  });
+  myDoc.moveDown(0.5);
+
+  myDoc.font('./fonts/Questrial-Regular.ttf').fontSize(12);
+  myDoc.text(submissionDetails.funding, {
+    align: 'justify'
+  });
+  myDoc.moveDown(0.5);
+
+  // Focus Areas
+
+  myDoc.font('./fonts/Sanchez-Italic.ttf').fontSize(20);
+  myDoc.text('Focus Areas:', {
+    align: 'left'
+  });
+  myDoc.moveDown(0.5);
+
+  myDoc.font('./fonts/Questrial-Regular.ttf').fontSize(12);
+  myDoc.text(submissionDetails.focus_areas, {
+    align: 'justify'
+  });
+  myDoc.moveDown(0.5);
+
+  // Comments
+
+  myDoc.font('./fonts/Sanchez-Italic.ttf').fontSize(20);
+  myDoc.text('Comments: ', {
+    align: 'left'
+  });
+  myDoc.moveDown(0.5);
+
+  myDoc.font('./fonts/Questrial-Regular.ttf').fontSize(12);
+  myDoc.text(submissionDetails.comments, {
+    align: 'justify'
+  });
+  myDoc.moveDown(0.5);
+
+  // ============
+  myDoc.addPage();
+
+  // Project Title
+  myDoc.font('./fonts/Sanchez-Italic.ttf').fontSize(20);
+  myDoc.text('Project Title: ', {
+    align: 'left'
+  });
+  myDoc.moveDown(0.5);
+
+  myDoc.font('./fonts/Questrial-Regular.ttf').fontSize(12);
+  myDoc.text(submissionDetails.project_title, {
+    align: 'justify'
+  });
+  myDoc.moveDown(0.5);
+
+  // Bio
+  myDoc.font('./fonts/Sanchez-Italic.ttf').fontSize(20);
+  myDoc.text('Bio: ', {
+    align: 'left'
+  });
+  myDoc.moveDown(0.5);
+
+  myDoc.font('./fonts/Questrial-Regular.ttf').fontSize(12);
+  myDoc.text(submissionDetails.bio, {
+    align: 'justify'
+  });
+  myDoc.moveDown(0.5);
+
+  // Contact Details:
+  myDoc.font('./fonts/Sanchez-Italic.ttf').fontSize(20);
+  myDoc.text('Contact Details:', {
+    align: 'left'
+  });
+
+  myDoc.font('./fonts/Questrial-Regular.ttf').fontSize(12);
+  myDoc.text('Name: '+submissionDetails.name, {
+    align: 'left'
+  });
+  myDoc.moveDown(0.5);
+  myDoc.text('Email: '+submissionDetails.email, {
+    align: 'left'
+  });
+  myDoc.moveDown(0.5);
+  myDoc.text('Phone: '+submissionDetails.phone, {
+    align: 'left'
+  });
+  myDoc.moveDown(0.5);
+
   myDoc.end();
 
 
-  // Stream contents to a file
-  // TODO: Fix issue with blank stream. End event is not firing. Generated file is blank.
-
+  writeStream.on('finish', function () {
+    var body = fs.createReadStream(filepath);
+    s3obj.upload({Body: body}).
+      on('httpUploadProgress', function(evt) { console.log(evt); }).
+      send(function(err, data) { console.log(err, data) });
+    console.log('Output file size:' + getFilesizeInBytes(filepath) + ' bytes');
+  });
 }
 
 /*
